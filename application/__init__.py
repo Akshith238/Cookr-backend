67a39b89f715170776be5bff34f611f9bea8b44f
Akshith238 <you@example.com>
2024-03-03 21:41:44 +0530

    nth

--- application/__init__.py
+++ application/__init__.py
@@ -14,4 +14,14 @@ loaded_pred_maintenance_model = load('models/predictive_maintenance_model.joblib
 rf_classifier = load('models/rf_model.pkl')
 tfidf_vectorizer = load('models/tfidf_vectorizer.pkl')
 
+loaded_rf_classifiers = {}
+attributes = ['diet', 'flavor_profile', 'course', 'state', 'region']
+for attribute in attributes:
+    classifier = load(f'models/{attribute}_classifier.joblib')
+    feature_selector = load(f'models/{attribute}_feature_selector.joblib')
+    loaded_rf_classifiers[attribute] = (classifier, feature_selector)
+
+# Load the TfidfVectorizer
+loaded_vectorizer = load('models/vectorizer.joblib')
+
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,9 @@
-from application import app, rf_classifier,tfidf_vectorizer,loaded_pred_maintenance_model
+from application import app, rf_classifier,tfidf_vectorizer,loaded_rf_classifiers,loaded_vectorizer,loaded_pred_maintenance_model
 from flask import jsonify, request
 import numpy as np
 import pandas as pd
+import psutil
+
 
 def generate_live_data():
     cpu_usage = float(np.random.uniform(0, 100))
@@ -18,6 +20,46 @@ def generate_live_data():
     
     return input_data
 
+def bytes_to_mb(bytes_value):
+    return bytes_value / (1024)
+
+
+def generate_live_data1():
+    cpu_usage = [psutil.cpu_percent()]
+    memory_utilization = [psutil.virtual_memory().percent]
+    network_info = psutil.net_io_counters()
+    
+    # Scale the network traffic values between 0 and 1000
+    network_traffic_in = [float(bytes_to_mb(network_info.bytes_recv))]
+    network_traffic_out = [float(bytes_to_mb(network_info.bytes_sent))]
+    
+    input_data = {
+        'cpu_usage': cpu_usage,
+        'memory_utilization': memory_utilization,
+        'network_traffic_in': network_traffic_in,
+        'network_traffic_out': network_traffic_out
+    }
+    
+    return input_data
+
+
+
+def predict_attributes(dish_name, dish_ingredients):
+    dish_combined = ' '.join([dish_name, dish_ingredients])
+    dish_vectorized = loaded_vectorizer.transform([dish_combined])
+    predicted_attributes = {}
+    for attribute, (classifier, feature_selector) in loaded_rf_classifiers.items():
+        selected_features = feature_selector.transform(dish_vectorized)
+        predicted_attribute = classifier.predict(selected_features)[0]
+        if predicted_attribute == "-1":
+            predicted_attribute = "Unknown"
+        predicted_attributes[attribute] = predicted_attribute
+    # Check for non-vegetarian ingredients
+    non_veg_ingredients = ["chicken", "mutton", "beef", "pork", "fish", "egg", "prawn", "lobster", "crab"]
+    if any(ing.lower() in dish_ingredients.lower() for ing in non_veg_ingredients):
+        predicted_attributes['diet'] = 'non vegetarian'
+    return predicted_attributes
+
 
 
 @app.route("/api/predictFood", methods=["POST"])
@@ -28,6 +70,19 @@ def foodPredict():
         dish_name_encoded = tfidf_vectorizer.transform([dish_name])
         prediction = rf_classifier.predict(dish_name_encoded)
         return jsonify(prediction.tolist()), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route("/api/predictFood2", methods=["POST"])
+def foodPredict2():
+    try:
+        # Parse request JSON
+        data = request.json
+        dish_name = data["dish_name"]
+        dish_ingredients = data["dish_ingredients"]
+        predicted_attributes = predict_attributes(dish_name, dish_ingredients)
+        return jsonify(predicted_attributes), 200
+    
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
